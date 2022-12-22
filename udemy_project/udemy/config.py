e88d61fb24ae73f50d02de303a09c80bce55b73a
bharathgowrabathina-cervello <118655871+bharathgowrabathina-cervello@users.noreply.github.com>
2022-12-22 15:52:55 +0530

    Update config.py

--- udemy_project/udemy/config.py
+++ udemy_project/udemy/config.py
@@ -1,17 +1,17 @@
+"""Environment Specific Config Values should be set here"""
+
 import os
-
-
 from urllib.parse import quote_plus as urlquote
 
 
 
 class BaseConfig(object):
     
+    # Flask variables
     FLASK_DEBUG = False
     TESTING = False
 
-    # developmentconfig=os.getenv('FLASK_ENV')
-
+    # sql db specific
     APP_DB_USER = os.getenv('APP_DB_USER')
     APP_DB_SECRET = os.getenv("APP_DB_SECRET")
     
@@ -22,15 +22,10 @@ class BaseConfig(object):
 
     SECRET_KEY=os.getenv('SECRET_KEY')
 
-    # SQLALCHEMY_DATABASE_URI = (
-    #     f"mssql+pymssql://{APP_DB_USER}:%s@{APP_DB_HOST}:{APP_DB_PORT}/{APP_DB_NAME}"
-    #     % quote_plus(APP_DB_SECRET)
-    # )
+  
     SQLALCHEMY_DATABASE_URI = (
         f"mssql+pymssql://{APP_DB_USER}:%s@{APP_DB_HOST}:{APP_DB_PORT}/{APP_DB_NAME}" % urlquote(str(APP_DB_SECRET))
     )
-    # print(SQLALCHEMY_DATABASE_URI)
-
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 
